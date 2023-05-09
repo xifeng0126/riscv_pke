6ca4580ded786257abecfed4ab9fdee2e05f1973
root <root@LAPTOP-GC4G62T0.localdomain>
2023-05-10 01:23:55 +0800

    work on lab1_challenge is done

--- kernel/syscall.h
+++ kernel/syscall.h
@@ -8,6 +8,9 @@
 #define SYS_user_base 64
 #define SYS_user_print (SYS_user_base + 0)
 #define SYS_user_exit (SYS_user_base + 1)
+#define SYS_user_backtrace (SYS_user_base + 2)
+
+#define handle_entry_offload(entry) (entry - 0xe)
 
 long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, long a7);
 
